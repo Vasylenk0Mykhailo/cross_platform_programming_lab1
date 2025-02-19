5b2f47c3aa7d474b61aed2b74c82a4809b128f02
Reiter <147085935+Vasylenk0Mykhailo@users.noreply.github.com>
2025-02-19 11:09:11 +0200

    add third task

--- myTest/src/app/tab3/tab3.page.ts
+++ myTest/src/app/tab3/tab3.page.ts
@@ -1,13 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonItem, IonLabel } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { MyHeaderComponent } from '../my-header/my-header.component';
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, MyHeaderComponent],
+  standalone: true,
+  imports: [FormsModule, IonInput, IonLabel, IonButton, IonHeader, IonToolbar, IonTitle, IonContent, MyHeaderComponent, IonItem, CommonModule]
 })
 export class Tab3Page {
+  matrixSize: number = 3;
+  matrix: number[][] = [];
+  diagonalSum: number = 0;
+  highlightYellow: boolean = false;
+
   constructor() {}
+
+  generateMatrix() {
+    this.matrix = [];
+    this.diagonalSum = 0;
+    this.highlightYellow = false;
+
+    for (let i = 0; i < this.matrixSize; i++) {
+      let row: number[] = [];
+      for (let j = 0; j < this.matrixSize; j++) {
+        const value = Math.floor(Math.random() * 100); 
+        row.push(value);
+      }
+      this.matrix.push(row);
+    }
+
+    let allOdd = true;
+    for (let i = 0; i < this.matrixSize; i++) {
+      this.diagonalSum += this.matrix[i][i];
+      if (this.matrix[i][i] % 2 === 0) {
+        allOdd = false;
+      }
+    }
+
+    this.highlightYellow = allOdd;
+  }
 }
